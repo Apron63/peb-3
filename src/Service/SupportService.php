d893261977e23b211357cc20eb1841de3aefbd93
Apron63 <apron63@yandex.ru>
2023-05-14 10:46:41 +0400

    Fix - Грамматическая ошибка в теме письма

--- src/Service/SupportService.php
+++ src/Service/SupportService.php
@@ -17,13 +17,13 @@ class SupportService
     {
         $this->bus->dispatch(new SendEmailMessage(
             'support@safety63.ru',
-            'Зпрос технической поддержки СДО',
+            'Запрос технической поддержки СДО',
             $this->composeMail($support)
         )); 
         
         $this->bus->dispatch(new SendEmailMessage(
             'info@safety63.ru',
-            'Зпрос технической поддержки СДО',
+            'Запрос технической поддержки СДО',
             $this->composeMail($support)
         ));
     }
