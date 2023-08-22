7730ba8143e414949790d6f58d4d27252805c2de
Apron63 <apron63@yandex.ru>
2023-08-22 23:34:27 +0400

    Fix - поправлено поведение фильтров в списке учебных материалов

--- assets/js/admin/course.js
+++ assets/js/admin/course.js
@@ -3,21 +3,27 @@ import $ from 'jquery'
 const parsedUrl = new URL(window.location)
 
 $('#course-type-select').on('change', function () {
-    const value = $('#course-type-select').val()
+    const value = $('#course-type-select').val() 
+
     if (value === '0') {
         parsedUrl.searchParams.delete('type')
     } else {
         parsedUrl.searchParams.set('type', value)
     }
+
+    parsedUrl.searchParams.delete('page')
     window.location = parsedUrl.href
 })
 
 $('#profile-type-select').on('change', function () {
     const value = $('#profile-type-select').val()
+    
     if (value === '0') {
         parsedUrl.searchParams.delete('profile')
     } else {
         parsedUrl.searchParams.set('profile', value)
     }
+
+    parsedUrl.searchParams.delete('page')
     window.location = parsedUrl.href
 })
