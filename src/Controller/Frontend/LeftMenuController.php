a3608806c930a7a3feb3738cf4810fb7ea6abe87
Apron63 <apron63@yandex.ru>
2023-11-09 20:58:22 +0400

    Throw 403 Exception if user not found in rendering leftMenu

--- src/Controller/Frontend/LeftMenuController.php
+++ src/Controller/Frontend/LeftMenuController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller\Frontend;
 
+use App\Entity\User;
 use App\Repository\PermissionRepository;
 use App\Service\MyProgramsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class LeftMenuController extends AbstractController
 {
@@ -20,6 +22,10 @@ class LeftMenuController extends AbstractController
     public function index(Request $request): Response
     {
         $user = $this->getUser();
+
+        if (! $user instanceof User) {
+            throw new AccessDeniedException('User access denied');
+        }
 
         return $this->render('frontend/left-menu/index.html.twig', [
             'activeItem' => $request->get('activeItem'),
