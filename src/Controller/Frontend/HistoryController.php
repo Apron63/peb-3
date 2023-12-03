5cdcfe080ef233c2a7ef0adc8871064d9adb4112
Apron63 <apron63@yandex.ru>
2023-12-03 22:25:25 +0400

    На странице редактирования пользователя удалены табы "Действия" и "Результат" как неактуальные

--- src/Controller/Frontend/HistoryController.php
+++ src/Controller/Frontend/HistoryController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use App\Repository\PermissionRepository;
 use App\Service\HistoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
