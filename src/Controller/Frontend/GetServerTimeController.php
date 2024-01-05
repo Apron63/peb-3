635540c7956e37ab404fd1e4300aaba4c6567d42
Apron63 <apron63@yandex.ru>
2024-01-06 01:06:06 +0400

    Исправлен отсчет времени для демоверсии

--- src/Controller/Frontend/GetServerTimeController.php
+++ src/Controller/Frontend/GetServerTimeController.php
@@ -14,4 +14,10 @@ class GetServerTimeController extends AbstractController
     {
         return (new Response())->setContent((new DateTime())->getTimestamp());
     }
+
+    #[Route('/demo/get-server-time/', name: 'app_frontend_demo_get_server_time', methods:'GET')]
+    public function indexDemo(): Response
+    {
+        return (new Response())->setContent((new DateTime())->getTimestamp());
+    }
 }
