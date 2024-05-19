a6dd449c95e982215692ff3e62918d9ecccae507
Apron63 <apron63@yandex.ru>
2024-05-19 16:04:47 +0400

    Fix - проверка длины имени файла

--- src/Form/Admin/LoadCourseType.php
+++ src/Form/Admin/LoadCourseType.php
@@ -6,7 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class LoadCourseType extends AbstractType
 {
@@ -24,8 +27,8 @@ class LoadCourseType extends AbstractType
                             'application/zip',
                         ],
                         'mimeTypesMessage' => 'Выбранный файл не является ZIP архивом',
-                        'filenameMaxLength' => 150,
-                    ])
+                    ]),
+                    new Callback([$this, 'validateFileNameMaxLength']),
                 ],
             ])
             ->add('submit', SubmitType::class, [
@@ -34,5 +37,15 @@ class LoadCourseType extends AbstractType
                     'class' => 'btn btn-outline-dark',
                 ],
             ]);
+    }
+
+    public function validateFileNameMaxLength(UploadedFile $file, ExecutionContextInterface $context): void
+    {
+        $originalFileName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $fileNameLength = mb_strlen($originalFileName);
+
+        if ($fileNameLength > 150) {
+            $context->addViolation('Длина имени файла: ' . $fileNameLength . ' символов, превышает допустимое значние 150 символов');
+        }
     }
 }
