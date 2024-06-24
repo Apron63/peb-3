3a69159256fac73f3834ecf04fc27a0fd1ace65e
Apron63 <apron63@yandex.ru>
2024-06-24 22:51:59 +0400

    Для поля "Длительность доступа" поправлен тип ввода как целое число

--- src/Form/Admin/PermissionEditType.php
+++ src/Form/Admin/PermissionEditType.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,7 +22,7 @@ class PermissionEditType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('duration', TextType::class, [
+            ->add('duration', IntegerType::class, [
                 'label' => 'Длительность',
                 'attr' => [
                     'class' => 'form-control',
