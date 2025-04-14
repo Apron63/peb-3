319c930fbf0ac5b2b0e6a3349f9509f2ef89ded4
Apron63 <apron63@yandex.ru>
2025-04-14 23:33:17 +0400

    Fix - в некоторых случаях могла произойти ошибка в дате назначения нового доступа

--- src/Entity/Permission.php
+++ src/Entity/Permission.php
@@ -264,7 +264,7 @@ class Permission
     {
         $creationDate = $this->createdAt ?? new DateTime();
 
-        return $creationDate->add(new DateInterval('P' . $this->duration . 'D'));
+        return (clone $creationDate)->add(new DateInterval('P' . $this->duration . 'D'));
     }
 
     public function getCheckedBy(): ?User
